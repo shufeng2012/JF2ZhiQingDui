492af442731c580824b4c0f76919a46b833b307f
shufeng2012 <277665039@qq.com>
2024-06-01 13:45:49 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+let jiaru_class = document.getElementById('jiaru_class')
+let jiaru_name = document.getElementById('jiaru_name')
+let jiaru_submit = document.getElementById('jiaru_submit')
---
+++ sy.js
@@ -0,0 +1,6 @@
+var myVariable = "Hello, World!";
+ 
+function updateContent() {
+    //document.getElementById("myDiv").innerHTML = myVariable;
+    alert('jsjs');
+}
