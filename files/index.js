7762907054f1c8e81f02dd7bbc6743a472a4acc0
shufeng2012 <277665039@qq.com>
2024-06-10 10:32:37 +0800

    main files

---
+++ files/index.js
@@ -0,0 +1,62 @@
+/*
+JavaScript函数
+*/
+
+//加入执勤队
+function JiaRu()
+{
+    //录入
+    alert('加入成功！');
+}
+
+//添加实习
+function ShiXi()
+{
+    //修改
+    alert('更改成功！');
+}
+
+//转正队员
+function ZhuanZheng()
+{
+    //修改
+    alert('修改成功');
+}
+
+//踢出队员
+function TiChu()
+{
+    //修改
+    alert('成功踢出！');
+}
+
+//查看队员
+function ChaKan()
+{
+    alert('以下是执勤队所有成员的名单');
+    //名单
+}
+
+/*
+JavaScript变量
+*/
+
+//jiaru.html
+var jiaru_class = document.getElementById('jiaru_class');
+var jiaru_name = document.getElementById('jiaru_name');
+
+//shixi.html
+var shixi_duiyuan_class = document.getElementById('shixi_duiyuan_class');
+var shixi_duiyuan_name = document.getElementById('shixi_duiyuan_name');
+var shixi_daoshi_class = document.getElementById('shixi_daoshi_class');
+var shixi_daoshi_name = document.getElementById('shixi_daoshi_name');
+
+//zhuangzheng.html
+var zhuangzheng_daoshi_class = document.getElementById('zhuangzheng_daoshi_class');
+var zhuangzheng_daoshi_name = document.getElementById('zhuangzheng_daoshi_name');
+var zhuangzheng_duiyuan_class = document.getElementById('zhuangzheng_duiyuan_class');
+var zhuangzheng_duiyuan_name = document.getElementById('zhuangzheng_duiyuan_name');
+
+//tichu.html
+var tichu_class = document.getElementById('tichu_class');
+var tichu_name = document.getElementById('tichu_name');
